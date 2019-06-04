03212974f575e764cc1b517cde0316fd31e9872a
W3234 <W3234@lab.aca>
2019-06-04 12:24:47 -0500

    Duración del video

--- app.rb
+++ app.rb
@@ -11,16 +11,28 @@ puts 'texto del title de sitio web'
 puts '  ' + doc.css('title')[0].text
 
 #puts doc.css('#watch-related')
+"""
 
 doc.css('#watch-related', '.video-list-item .content-wrapper').each do |link|
   puts '+++++++++++++++++++++++++++++++++++++++++++++++++++'
   puts link
 end
 
+puts '+++++++++++++++++++++++++++++++++++++++++++++++++++'
+
 doc.css('#watch-related', '.video-list-item .thumb-wrapper a img').each do |link|
-  puts '+++++++++++++++++++++++++++++++++++++++++++++++++++'
-  puts link
   puts '1 ------'
-  puts link.attribute('width')
+  puts link
+  puts link.attribute('data-thumb')
   puts '2 ------'
+end
+
+"""
+
+doc.css('#watch-related', '.video-list-item').each do |item_list|
+  # link del video
+  # puts item_list.css('.content-wrapper a').attribute('href')
+  # duracion del video
+  # puts item_list.css('.thumb-wrapper a').text
+  #item_list.css('.thumb-wrapper')
 end
