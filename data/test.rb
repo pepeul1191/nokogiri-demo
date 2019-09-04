a0d4efc5527a301c3db4b69d3130d946bad677b7
Pepe Valdivia <jvaldivia@softweb.pe>
2019-09-03 20:22:30 -0500

    Insertar profes

--- data/test.rb
+++ data/test.rb
@@ -14,9 +14,9 @@ class Carrers < Sequel::Model(DB[:carrers])
 
 end
 
-# class Teachers < Sequel::Model(DB[:teachers])
+class Teachers < Sequel::Model(DB[:teachers])
 
-# end
+end
 
 # functions
 
@@ -30,13 +30,43 @@ end
 def insert_carrers
   file = File.read('profes.json')
   teachers = JSON.parse(file)
-  teachers.each do |teacher|
-    teacher['carrers'].each do |carrer|
-      if Carrers.find(name: carrer['name']) == nil
-        Carrers.create(name: carrer['name'])
+  DB.transaction do
+    begin
+      teachers.each do |teacher|
+        teacher['carrers'].each do |carrer|
+          if Carrers.find(name: carrer['name']) == nil
+            Carrers.create(name: carrer['name'])
+          end
+        end
       end
+    rescue Exception => e
+      Sequel::Rollback
+      puts e
     end
   end
 end
 
-insert_carrers
+# insert_carrers
+
+def insert_teachers
+  file = File.read('profes.json')
+  teachers = JSON.parse(file)
+  DB.transaction do
+    begin
+      teachers.each do |teacher|
+        full_name = teacher['name']
+        name_array = full_name.split(', ')
+        names = name_array[1]
+        last_names = name_array[0]
+        if Teachers.find(names: names, last_names: last_names) == nil
+          Teachers.create(names: names, last_names: last_names)
+        end
+      end
+    rescue Exception => e
+      Sequel::Rollback
+      puts e
+    end
+  end
+end
+
+insert_teachers
